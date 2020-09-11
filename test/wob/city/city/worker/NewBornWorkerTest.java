b2983be51cd0bc62b54ca4a4821f6558b94906e1
romeomihalovics <romeo@mihalovics.com>
2020-09-11 09:24:07 +0200

    bugfix, tests

--- test/wob/city/city/worker/NewBornWorkerTest.java
+++ test/wob/city/city/worker/NewBornWorkerTest.java
@@ -1,14 +1,14 @@
-package wob.city.worker;
+package wob.city.city.worker;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import wob.city.city.worker.NewBornWorker;
 import wob.city.food.abstraction.Food;
 import wob.city.person.abstraction.Person;
+import wob.city.person.object.Boy;
 import wob.city.city.City;
 import wob.city.person.object.Girl;
 import wob.city.food.object.Meat;
-import wob.city.person.object.Woman;
-import wob.city.person.worker.AgingWorker;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,28 +18,22 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class AgingWorkerTest {
+class NewBornWorkerTest {
     @Test
-    @DisplayName("Aging worker should increase age by one and make child into adult if it reaches the age of 18 or kill it with a 10% chance [or with a 25% chance if over age 60]")
-    void AgingWorkerShouldDo() {
-        Person person = new Girl();
-        person.setAge(17);
+    @DisplayName("New Born Worker should add one kid (boy or girl) with age 1 to people list in city")
+    void newBornWorkerShouldDo() {
         List<Person> people = new ArrayList<>();
-        people.add(person);
         List<Person> syncedPeople = Collections.synchronizedList(people);
 
         List<Food> foods = Collections.singletonList(new Meat(Arrays.asList("Sausage", "14", "1", "28")));
 
         City city = new City("Test City", syncedPeople, foods);
-        city.getPeople().forEach(p -> p.setLocation(city));
 
-        person.setWorkers();
+        NewBornWorker newBornWorker = new NewBornWorker(city);
+        newBornWorker.run();
 
-        AgingWorker agingWorker = new AgingWorker(person);
-        agingWorker.run();
-
-        assertEquals(18, person.getAge());
-        assertTrue(people.size() == 1 || people.size() == 0);
-        assertTrue(people.size() == 0 || people.get(0) instanceof Woman);
+        assertEquals(1, city.getPeople().size());
+        assertEquals(1, city.getPeople().get(0).getAge());
+        assertTrue(city.getPeople().get(0) instanceof Boy || city.getPeople().get(0) instanceof Girl);
     }
 }
--- test/wob/city/controller/services/FoodLoaderTest.java
+++ test/wob/city/controller/services/FoodLoaderTest.java
@@ -1,4 +1,4 @@
-package wob.city.services;
+package wob.city.controller.services;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
